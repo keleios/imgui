c71a0c0a3855f8ee5e698c0995801fdae645a879
keleios <brendan.geiger@gmail.com>
2023-10-30 23:09:43 -0700

    - Updated .gitignore to block vcxproj files - Updated premake to actually include all the imgui files.

--- premake5.lua
+++ premake5.lua
@@ -10,32 +10,35 @@ project "ImGui"
     files
     {
         "imconfig.h",
-        "imgui.h",
-        "imgui_draw",
-        "imgui_internal",
+        "imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+		"imgui_tables.cpp",
         "imgui_widgets.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-        "imgui_demo.cpp",
---        "../imgui-sfml/imconfig-SFML.h"
+        
+        "../imgui-sfml/imconfig-SFML.h"
     }
 
---    includedirs
---    {
---        "../SFML/include"
---    }
+    includedirs
+    {
+        "../SFML/include"
+    }
 
---    defines
---    {
---        "IMGUI_USER_CONFIG=\"../imgui-sfml/imconfig-SFML.h\"",
---    }
+    defines
+    {
+        "IMGUI_USER_CONFIG=\"../imgui-sfml/imconfig-SFML.h\"",
+    }
 
---    links
---    {
---        "sfml-system",
---        "sfml-graphics",
---    }
+    links
+    {
+        "sfml-system",
+        "sfml-graphics",
+    }
 
     filter "system:windows"
         systemversion "latest"
